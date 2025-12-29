9d316c55bbf2dc0031c2d791f2e88558d04781bd
Amit K <androidak0r@gmail.com>
2025-12-29 13:12:51 +0000

    build: :heavy_plus_sign: Added initial dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,53 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
+import icon from 'astro-icon';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  image: {
+    responsiveStyles: true,
+    layout: "constrained"
+  },
+
+  experimental: {
+    contentIntellisense: true,
+    fonts: [
+      {
+        name: "Poppins",
+        cssVariable: "--font-headings",
+        provider: fontProviders.fontsource(),
+        weights: [400, 500, 600, 700],
+        fallbacks: ["sans-serif"],
+      },
+      {
+        name: "Inter",
+        cssVariable: "--font-secondary",
+        provider: fontProviders.fontsource(),
+        weights: [400, 500, 600, 700],
+        fallbacks: ["sans-serif"],
+      },
+      {
+        name: "Rubik",
+        cssVariable: "--font-primary",
+        provider: fontProviders.fontsource(),
+        weights: [400, 500, 600, 700],
+        fallbacks: ["sans-serif"],
+      },
+      {
+        name: "JetBrains Mono",
+        cssVariable: "--font-code",
+        provider: fontProviders.fontsource(),
+        weights: [400, 500, 600, 700],
+        fallbacks: ["monospace"],
+      },
+    ]
+  },
+
+  integrations: [mdx(), icon()]
+});
